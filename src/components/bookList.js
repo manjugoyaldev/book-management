9ed906265bba091b39b31ca9412c0b056bc42014
Manju Goyal <manju6goyal@gmail.com>
2021-06-07 13:13:06 +0530

    Initial commit

---
+++ src/components/bookList.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import '../styles/bookList.css';
+
+const BookList = () => {
+  return (
+    <div className={'bg'}>
+      Hello
+    </div>
+  )
+}
+
+export default BookList;
